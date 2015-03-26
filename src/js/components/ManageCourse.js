4004dd435844a1b4fb663072bae9729a802cfa03
Luke Leheny <lleheny@umass.edu>
2015-03-26 02:50:55 -0400

    making some progress

--- src/js/components/ManageCourse.js
+++ src/js/components/ManageCourse.js
@@ -1,6 +1,19 @@
-var React = require('react');
+var React  = require('react');
 var Router = require('react-router');
+var Course = require('./courses/Course');
 
 var ManageCourse = React.createClass({
 	
+	propTypes: {
+    course_id: React.PropTypes.number.isRequired,
+    students: React.PropTypes.array.isRequired
+  },
+  
+	render: function() {
+		return (
+			<div id='manage-course'>
+        
+      </div>
+		);
+	}
 });
