392149f59fc637afe64c2d5aeec72bdc9071d862
Santiago <sdelatorrepi@umass.edu>
2015-04-24 00:25:21 -0400

    added createComment (untested)

--- database/comments.js
+++ database/comments.js
@@ -1,3 +1,26 @@
-var Course = require('./models/course');
+var Comment = require('./models/comment');
 exports.lecture = require('./lectures');
 exports.comment = require('./comments');
+
+/*
+ * Methd that creates a comment
+ */
+exports.createComment = function(lecture_id, user_id, content, post_date) {
+    Comment.find({
+        lecture: lecture_id,
+        autho: user_id,
+        content,
+        date: post_date
+    }, function(err, comment) {
+        if (err) {
+            callback(err);
+        } else {
+            Comment.create({
+                lecture: lecture_id,
+                author: user_id,
+                date: post_date,
+                content: content
+            });
+        }
+    });
+};
