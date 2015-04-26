208614841fb0044b63f04f06862c11df96d8e28f
Albert <albertung@gmail.com>
2015-04-25 20:17:14 -0400

    adding createCourse test

---
+++ test/course/index.js
@@ -0,0 +1,52 @@
+var should = require('should');
+var request = require('supertest');
+
+var database = require('../../database/index.js');
+
+var url = 'http://localhost:3000';
+
+describe('Course', function() {
+	
+	var course_info = {
+		department : "Computer Science",
+		courseNumber : "497s",
+		courseTitle : "Web Scalability",
+		semester : "Spring",
+		year : "2015",
+		instructor : "instructor@umass.edu"
+	};
+
+
+	before(function(done) {
+		database.course.dropCoursesDatabase(function() {
+			done();
+		});
+	});
+
+	describe('Valid Call', function() {
+		it('Creating course', function(done) {								
+			request(url)
+				.post('/course')
+				.send(course_info)
+				.end(function(err, res) {
+					console.log(res.body.data);
+					res.body.status.should.equal('success');
+					done();
+				});
+		});
+
+		// it('Get course', function(done) {
+		// 	request(url)
+		// 		.get('/course/#');
+		// 		.end(function(err, res) {
+		// 			done();
+		// 		});
+		// });
+
+	});
+
+	// describe('Invalid calls', function() {
+		
+	// });
+
+});
