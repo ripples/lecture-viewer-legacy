f0005780652ab274d2437a083941d36d4228b88f
Albert <albertung@gmail.com>
2015-04-28 00:14:41 -0400

    adding to roster test file

---
+++ test/course/roster.js
@@ -0,0 +1,18 @@
+var should = require('should');
+var request = require('supertest');
+
+var database = require('../../database/index.js');
+
+var url = 'http://localhost:3000';
+
+describe('Roster', function() {
+
+	before(function(done) {
+		done();
+	});
+
+	it('Get roster from course', function(done) {
+		done();
+	});
+
+});
