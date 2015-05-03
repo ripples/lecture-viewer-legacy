3860270a07285ff3037bc6fa240924da8b2f7ce7
Albert <albertung@gmail.com>
2015-05-03 14:59:50 -0400

    adding auth to courses routes, updating tests to include auth tests

--- routes/course/index.js
+++ routes/course/index.js
@@ -15,16 +15,78 @@ var auth = require('../../authentication');
 	router.post('/', auth.verify, function(req,res) {
 		//Check if all required parameters are present
 	
-		if(req.body.department && req.body.course_number && req.body.course_title && req.body.semester && req.body.year && req.body.instructor_email) {
+		if(req.user.role != "admin") {
 			
-			// Attempts to create a course record in database
-			
-			database.course.createCourse(req.body.department, req.body.course_number, req.body.course_title, req.body.semester, req.body.year, req.body.instructor_email, function(err, course) {
+			res.sendFail("Not an admin");
+		
+		} else {
+			if(req.body.department && req.body.course_number && req.body.course_title && req.body.semester && req.body.year && req.body.instructor_email) {
 				
-				// If no error, send back course data
+				// Attempts to create a course record in database
 				
-				if(!err) {
+				database.course.createCourse(req.body.department, req.body.course_number, req.body.course_title, req.body.semester, req.body.year, req.body.instructor_email, function(err, course) {
+					
+					// If no error, send back course data
+					
+					if(!err) {
 
+						var resCourse = {};
+
+						resCourse.department = course.department;
+						resCourse.course_number = course.courseNumber;
+						resCourse.course_title = course.courseTitle;
+						resCourse.semester = course.semester;
+						resCourse.year = course.year;
+						resCourse.instructor_emails = course.instructors;	
+						resCourse.course_id = course._id;
+						resCourse.lectures = course.lectures;
+
+						res.sendSuccess(resCourse);
+
+					} else {
+						
+						res.sendFail(err);
+					}
+				});
+			
+			} else {
+				
+				res.sendFail("Incorrect parameters");
+			
+			} 
+		}
+});
+
+//Get course
+router.get('/:course_id', auth.verify, function(req,res) {
+	
+	//Get course info from database
+
+	if(req.user.role != "admin") {
+
+		res.sendFail("Not an admin");
+		
+	} else {
+		if(req.params.course_id == undefined) { 
+
+			res.sendFail("No valid course_id parameter");
+
+		}
+		else if(validator.isMongoId(req.params.course_id) == false) {
+			
+			res.sendFail("Course ID is not a valid MongoID");
+		
+		}
+		else {
+			
+			database.course.getCourseById(req.params.course_id, function(err, course) {
+				
+				if(err) {
+
+					res.sendFail(err);
+
+				} else {
+					
 					var resCourse = {};
 
 					resCourse.department = course.department;
@@ -37,60 +99,10 @@ var auth = require('../../authentication');
 					resCourse.lectures = course.lectures;
 
 					res.sendSuccess(resCourse);
-
-				} else {
-					
-					res.sendFail(err);
 				}
 			});
-		
-		} else {
-			
-			res.sendFail("Incorrect parameters");
-		
-		} 
-});
-
-//Get course
-router.get('/:course_id', auth.verify, function(req,res) {
-	
-	//Get course info from database
-
-	if(req.params.course_id == undefined) { 
-
-		res.sendFail("No valid course_id parameter");
-
-	}
-	else if(validator.isMongoId(req.params.course_id) == false) {
-		
-		res.sendFail("Course ID is not a valid MongoID");
-	
-	}
-	else {
-		
-		database.course.getCourseById(req.params.course_id, function(err, course) {
-			
-			if(err) {
-
-				res.sendFail(err);
-
-			} else {
-				
-				var resCourse = {};
-
-				resCourse.department = course.department;
-				resCourse.course_number = course.courseNumber;
-				resCourse.course_title = course.courseTitle;
-				resCourse.semester = course.semester;
-				resCourse.year = course.year;
-				resCourse.instructor_emails = course.instructors;	
-				resCourse.course_id = course._id;
-				resCourse.lectures = course.lectures;
-
-				res.sendSuccess(resCourse);
-			}
-		});
-	}
+		}
+	}	
 });
 
 //Edit course
@@ -144,38 +156,45 @@ router.put('/:course_id', auth.verify, function(req,res) {
 //Delete course
 router.delete('/:course_id', auth.verify, function(req,res) {
 	
-	if(req.params.course_id == undefined) {
+	if(req.user.role != "admin") {
 
-		res.sendFail("No valid course_id parameter");
+		res.sendFail("Not an admin");
 
-	}
-	else if(validator.isMongoId(req.params.course_id) == false) {
-		res.sendFail("Course ID is not a valid MongoID");
-	}
-	else {
-		database.course.deleteCourseById(req.params.course_id, function(err, course) {
-			
-			if(err) {
+	} else {
 
-				res.sendFail(err);
+		if(req.params.course_id == undefined) {
 
-			} else {
+			res.sendFail("No valid course_id parameter");
 
-				var resCourse = {};
+		}
+		else if(validator.isMongoId(req.params.course_id) == false) {
+			res.sendFail("Course ID is not a valid MongoID");
+		}
+		else {
+			database.course.deleteCourseById(req.params.course_id, function(err, course) {
+				
+				if(err) {
 
-				resCourse.department = course.department;
-				resCourse.course_number = course.courseNumber;
-				resCourse.course_title = course.courseTitle;
-				resCourse.semester = course.semester;
-				resCourse.year = course.year;
-				resCourse.instructor_emails = course.instructors;	
-				resCourse.course_id = course._id;
+					res.sendFail(err);
 
-				res.sendSuccess(resCourse);
+				} else {
 
-			}
+					var resCourse = {};
 
-		});
+					resCourse.department = course.department;
+					resCourse.course_number = course.courseNumber;
+					resCourse.course_title = course.courseTitle;
+					resCourse.semester = course.semester;
+					resCourse.year = course.year;
+					resCourse.instructor_emails = course.instructors;	
+					resCourse.course_id = course._id;
+
+					res.sendSuccess(resCourse);
+
+				}
+
+			});
+		}
 	}
 });
 
--- test/course/index.js
+++ test/course/index.js
@@ -3,6 +3,7 @@ var request = require('supertest');
 var assert = require('assert');
 
 var database = require('../../database/index.js');
+var helper = require('../helper.js');
 
 var url = 'http://localhost:3000';
 
@@ -17,161 +18,149 @@ describe('Course', function() {
 		instructor_email : "instructor@umass.edu"
 	};
 
-	var course_info2 = {
-		department : "Computer Science",
-		course_number : "497s",
-		course_title : "Web Scalability",
-		semester : "Spring",
-		year : 2015,
-		instructor_email : "Joe@umass.edu"
-	};
+	// var course_info2 = {
+	// 	department : "Computer Science",
+	// 	course_number : "497s",
+	// 	course_title : "Web Scalability",
+	// 	semester : "Spring",
+	// 	year : 2015,
+	// 	instructor_email : "Joe@umass.edu"
+	// };
 
-	var course_info3 = {
-		department : "Finance",
-		course_number : "301",
-		course_title : "Investment",
-		semester : "Spring",
-		year : 2007,
-		instructor_email : "Martin@umass.edu"
-	};
+	// var course_info3 = {
+	// 	department : "Finance",
+	// 	course_number : "301",
+	// 	course_title : "Investment",
+	// 	semester : "Spring",
+	// 	year : 2007,
+	// 	instructor_email : "Martin@umass.edu"
+	// };
 
-	var empty_parameter_course_info = {
-		department : "",
-		course_number : "301",
-		course_title : "Investment",
-		semester : "Spring",
-		year : 2007,
-		instructor_email : "Test@umass.edu"
-	};
+	// var empty_parameter_course_info = {
+	// 	department : "",
+	// 	course_number : "301",
+	// 	course_title : "Investment",
+	// 	semester : "Spring",
+	// 	year : 2007,
+	// 	instructor_email : "Test@umass.edu"
+	// };
 
-	var missing_parameter_course_info = {
-		course_number : "301",
-		course_title : "Investment",
-		semester : "Spring",
-		year : 2007,
-		instructor_email : "Steve@umass.edu"
-	};
+	// var missing_parameter_course_info = {
+	// 	course_number : "301",
+	// 	course_title : "Investment",
+	// 	semester : "Spring",
+	// 	year : 2007,
+	// 	instructor_email : "Steve@umass.edu"
+	// };
+
+	// var course_id = "";
+	// var new_course_id = "";
+	// var invalid_course_id = "test";
 
 	var course_id = "";
-	var new_course_id = "";
-	var invalid_course_id = "test";
-
-	var login_admin_auth = "";
+    var login_admin_id = "";
+    var login_admin_auth = "";
 
 	before(function(done) {
 		database.course.dropCoursesDatabase(function() {
-			// you will have to create a user here?
-
-
-		// request(url)
-		// 	.post('/course')
-		// 	.send(course_info2)
-		// 	.end(function(err, res) {
-		// 		if(err) {
-		// 			return done(err);
-		// 		} else {
-		// 			console.log(res.body.data);
-		// 			res.body.status.should.equal('success');
-		// 			course_id = res.body.data.course_id;
-		// 			res.body.data.department.should.equal('Computer Science');
-		// 			res.body.data.course_number.should.equal('497s');
-		// 			res.body.data.course_title.should.equal('Web Scalability');
-		// 			res.body.data.semester.should.equal('Spring');
-		// 			res.body.data.year.should.equal(2015);
-		// 			assert.deepEqual(res.body.data.instructor_emails, ['Joe@umass.edu'])
-		// 			assert.deepEqual(res.body.data.lectures, []);
-		// 			done();
-		// 		}
-		// 	});
+			database.user.dropUserDatabase(function() {
+				helper.createAdminAndLogin(function(err, user) {
+					login_admin_auth = user.token;
+	                login_admin_id = user.user_id;
+	                done();
+				});
+			});
 		});
-		done();
 	});
 
-	describe('Valid Call', function() {
-		// you will have to check for auth in each test
+
+	describe('Valid call', function() {
+		
+		it('Creating a new course', function(done) {
+			request(url)
+				.post('/course')
+				.set('Authorization', login_admin_auth)
+				.send(course_info)
+				.end(function(err, res) {
+					if(err) {
+						return done(err);
+					} else {
+						res.body.status.should.equal('success');
+						course_id = res.body.data.course_id;
+						res.body.data.department.should.equal('Psychology');
+						res.body.data.course_number.should.equal('101');
+						res.body.data.course_title.should.equal('Intro to Psychology');
+						res.body.data.semester.should.equal('Fall');
+						res.body.data.year.should.equal(2003);
+						assert.deepEqual(res.body.data.instructor_emails, ['instructor@umass.edu'])
+						assert.deepEqual(res.body.data.lectures, []);
+						done();
+					}
+				});
+		});
+
+		it('Creating a course that already exists', function(done) {
+			request(url)
+				.post('/course')
+				.set('Authorization', login_admin_auth)
+				.send(course_info)
+				.end(function(err, res) {
+					if(err) {
+						return done(err);
+					} else {
+						res.body.status.should.equal('fail');
+						res.body.data.message.should.equal('Psychology 101 Fall 2003 already exists.');
+						done();
+					}
+				});
+		});
+
+		it('Get a course', function(done) {
+			request(url)
+				.get('/course/' + course_id)
+				.set('Authorization', login_admin_auth)
+				.end(function(err, res) {
+					res.body.status.should.equal('success');
+					res.body.data.course_id.should.equal(course_id);
+					res.body.data.department.should.equal('Psychology');
+					res.body.data.course_number.should.equal('101');
+					res.body.data.course_title.should.equal('Intro to Psychology');
+					res.body.data.semester.should.equal('Fall');
+					res.body.data.year.should.equal(2003);
+					assert.deepEqual(res.body.data.instructor_emails, ['instructor@umass.edu'])
+					assert.deepEqual(res.body.data.lectures, []);
+					done();
+				});
+		}); 
+
+		it('Delete a course', function(done) {
+			request(url)
+				.delete('/course/' + course_id)
+				.set('Authorization', login_admin_auth)
+				.end(function(err, res) {
+					if(err) {
+						return done(err);
+					} else {
+						res.body.status.should.equal('success');
+						res.body.data.department.should.equal('Psychology');
+						res.body.data.course_number.should.equal('101');
+						res.body.data.course_title.should.equal('Intro to Psychology');
+						res.body.data.semester.should.equal('Fall');
+						res.body.data.year.should.equal(2003);
+						assert.deepEqual(res.body.data.instructor_emails, ['instructor@umass.edu'])
+						res.body.data.course_id.should.equal(course_id);
+						done();
+					}
+				});
+		});
 
 
-		// it('Creating new course', function(done) {										
-		// 	request(url)
-		// 		.post('/course')
-		// 		.send(course_info)
-		// 		.end(function(err, res) {
-		// 			if(err) {
-		// 				return done(err);
-		// 			} else {
-		// 				console.log(res.body.data);
-		// 				new_course_id = res.body.data.course_id;
-		// 				res.body.status.should.equal('success');
-		// 				res.body.data.department.should.equal('Psychology');
-		// 				res.body.data.course_number.should.equal('101');
-		// 				res.body.data.course_title.should.equal('Intro to Psychology');
-		// 				res.body.data.semester.should.equal('Fall');
-		// 				res.body.data.year.should.equal(2003);
-		// 				assert.deepEqual(res.body.data.instructor_emails, ['instructor@umass.edu']);
-		// 				assert.deepEqual(res.body.data.lectures, []);
-		// 				done();
-		// 			}
-		// 		});
-		// });
 
-	// 	it('Creating a course that already exists', function(done) {									// NEEDS WORK - doesn't find duplicates!!! - need to fix courses db call
-	// 		request(url)
-	// 			.post('/course')
-	// 			.send(course_info2)
-	// 			.end(function(err, res) {
-	// 				if(err) {
-	// 					return done(err);
-	// 				} else {
-	// 					res.body.status.should.equal('success');
-	// 					res.body.data.department.should.equal('Computer Science');
-	// 					res.body.data.course_number.should.equal('497s');
-	// 					res.body.data.course_title.should.equal('Web Scalability');
-	// 					res.body.data.semester.should.equal('Spring');
-	// 					res.body.data.year.should.equal(2015);
-	// 					assert.deepEqual(res.body.data.instructor_emails, ['Joe@umass.edu']);
-	// 					assert.deepEqual(res.body.data.lectures, []);
-	// 					res.body.data.course_id.should.equal(course_id);								// this creates 2 of the same courses - eed to fix this in the db 
-	// 					done();
-	// 				}
-	// 			});
-	// 	});
+	});
+});
 
-	// 	it('Delete course', function(done) {
-	// 		request(url)
-	// 			.delete('/course/' + new_course_id)
-	// 			.end(function(err, res) {
-	// 				res.body.status.should.equal('success');
-	// 				res.body.data.department.should.equal('Psychology');
-	// 				res.body.data.course_number.should.equal('101');
-	// 				res.body.data.course_title.should.equal('Intro to Psychology');
-	// 				res.body.data.semester.should.equal('Fall');
-	// 				res.body.data.year.should.equal(2003);					
-	// 				res.body.data.course_id.should.equal(new_course_id);
-	// 				assert.deepEqual(res.body.data.instructor_emails, ['instructor@umass.edu']);
-	// 				done();
-	// 			});
-	// 	});
-
-	// 	it('Get course', function(done) {
-	// 		request(url)
-	// 			.get('/course/' + course_id)
-	// 			.end(function(err, res) {
-	// 				if(err) {
-	// 					return done(err);
-	// 				} else {
-	// 					res.body.status.should.equal('success');
-	// 					res.body.data.department.should.equal('Computer Science');
-	// 					res.body.data.course_number.should.equal('497s');
-	// 					res.body.data.course_title.should.equal('Web Scalability');
-	// 					res.body.data.semester.should.equal('Spring');
-	// 					res.body.data.year.should.equal(2015);
-	// 					res.body.data.course_id.should.equal(course_id);
-	// 					assert.deepEqual(res.body.data.instructor_emails, ['Joe@umass.edu']);
-	// 					assert.deepEqual(res.body.data.lectures, []);
-	// 					done();
-	// 				}
-	// 			});
-	// 	});
+// invalid tests - you have to try to do all courses stuff with student_auth - should fail
+// invalid tests - you have to try passing in back parameters
 
 	// 	it('Edit course', function(done) {											
 	// 		request(url)
@@ -299,5 +288,5 @@ describe('Course', function() {
 	// 				}
 	// 			});
 	// 	});
-	});
-});
+	// });
+// });
